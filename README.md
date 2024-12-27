3ef6a166a3688720620dc55841ed8f3a53e1a313
Karan Hansraj <hansrajkaran3@gmail.com>
2024-12-27 16:49:37 +0530

    main

---
+++ README.md
@@ -0,0 +1,43 @@
+```markdown
+# PexelStock
+
+A simple image search engine built using HTML, CSS, and JavaScript, utilizing the Unsplash API.
+
+## Features
+
+* Search for images using a keyword
+* Display a gallery of images with download links
+* Load more images on demand
+
+## How it works
+
+1. The user inputs a keyword in the search bar and presses Enter or clicks the search button.
+2. The application sends a request to the Unsplash API with the keyword and retrieves a list of images.
+3. The images are displayed in a gallery with a download link for each image.
+4. The user can load more images by clicking the "More" button.
+
+## Technologies used
+
+* HTML5
+* CSS3
+* JavaScript (ES6+)
+* Unsplash API
+
+## API Key
+
+Please note that you need to replace the `API_KEY` variable in the `script.js` file with your own Unsplash API key.
+
+## Installation
+
+1. Clone the repository using `git clone https://github.com/your-username/pexelstock.git`
+2. Open the `index.html` file in a web browser to use the application.
+
+## Contributing
+
+Contributions are welcome! If you find any bugs or have suggestions for improvement, please open an issue or submit a pull request.
+
+
+## Acknowledgments
+
+* Unsplash API for providing a vast collection of high-quality images.
+* Codeium for providing a great AI-powered code completion tool.
---
+++ src/script.js
@@ -0,0 +1,73 @@
+let input = document.querySelector(".search-box input");
+let searchBtn = document.querySelector(".btn button");
+let images = document.querySelector(".images");
+let moreButton = document.querySelector("#load");
+
+const API_KEY = `lXEwEMzaOBKAQObwL_xqmMHAxwS_PXfWWIqtv3AvcAE`;
+let page = 1;
+let keyword = "";
+
+function download(url) {
+  console.log(url);
+  fetch(url)
+    .then((res) => res.blob())
+    .then((file) => {
+      let a = document.createElement("a");
+      a.href = URL.createObjectURL(file);
+      a.download = new Date().getTime();
+      a.click();
+    })
+    .catch(() => {
+      alert("Failed download");
+    });
+}
+
+async function getResponse() {
+  keyword = input.value;
+
+  let url = `https://api.unsplash.com/search/collections?page=${page}&query=${keyword}%3E&client_id=${API_KEY}&per_page=12`;
+  let response = await fetch(url);
+
+  let data = await response.json();
+  let results = data.results;
+  if (page == 1) {
+    images.innerHTML = "";
+  }
+  moreButton.style.display = "block";
+
+  //   console.log(results);
+  results.map((result) => {
+    let li = document.createElement("li");
+    li.classList.add("image");
+    let html = `<img src="${result.preview_photos[0].urls.small}" alt="img" class="photo"> 
+    <div class="details">
+                <div class="user">
+                    <img src="img/camera.svg" alt="img">
+                <span>${result.title}</span>
+                </div>
+                <div class="download" onclick=download("${result.preview_photos[0].urls.small}")>
+                    <img src="img/download.svg" alt="">
+                </div>
+            </div>`;
+    li.innerHTML = html;
+    images.appendChild(li);
+  });
+
+  //   console.log(data);
+}
+input.addEventListener("keyup", (e) => {
+  page = 1;
+  if (e.key == "Enter") {
+    getResponse();
+  }
+});
+
+searchBtn.addEventListener("click", () => {
+  page = 1;
+  getResponse();
+});
+
+moreButton.addEventListener("click", () => {
+  page++;
+  getResponse();
+});
